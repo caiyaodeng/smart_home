bd4efa70395f99f109de41968760106623a02b52
caiyaodeng <caiyaodeng@gmail.com>
2016-05-05 23:53:06 +0800

    change polling time to 60/s

--- smart_home/polling_machine.cpp
+++ smart_home/polling_machine.cpp
@@ -23,7 +23,7 @@ void PollingMachine::setPollingObj(std::list <ReadyDevice> *readyDeviceList, Dal
 
 void *PollingMachine::startPolling(void *arg) {
     while (true) {
-        sleep(5);
+        sleep(60);
         ((PollingMachine *)arg)->pollingTask();
     }
     return nullptr;
