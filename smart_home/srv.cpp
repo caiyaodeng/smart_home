345aeea1d6b5700beef8bad78d02346a803fdb6b
CaiYaodeng <caiyaodeng@gmail.com>
2016-05-10 09:36:25 -0400

    fix path of dynamic link library .

--- smart_home/srv.cpp
+++ smart_home/srv.cpp
@@ -9,7 +9,7 @@
 #include "../net/net_work.h"
 #include "message_parser.h"
 #include "bll.h"
-//#include "leveldb/db.h" 
+#include "leveldb/db.h" 
 using namespace std;
 
 int main () {
@@ -27,12 +27,12 @@ int main () {
     objNet->init();
     objBll->init();
 
-     /*leveldb::DB      *db;    
+     leveldb::DB      *db;    
         leveldb::Options  options;    
         options.create_if_missing = true;
     leveldb::Status status = leveldb::DB::Open(options,"/tmp/testdb", &db);    
         assert(status.ok());
-        */
+        
 
     while(true) {
         iTaskId = objNet->pull_msg(pUndoMsg, &iUndoMsgLength, pDoneMsg, pDoneMsgLength);
